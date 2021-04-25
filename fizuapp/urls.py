4459f762feb4f2c3d4eee14b14accf675f2a37fa
Vukasin Felbab <vukasindfelbab@gmail.com>
2021-04-25 19:30:27 +0200

    before vuejs

--- fizuapp/urls.py
+++ fizuapp/urls.py
@@ -3,7 +3,35 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('blank.html', views.blank, name='blank'),
+    path('buttons.html', views.buttons, name='buttons'),
+    path('cards.html', views.cards, name='cards'),
+    path('charts.html', views.charts, name='charts'),
+    path('forgot-password.html', views.forgot_password, name='forgot_password'),
+    path('login.html', views.login, name='login'),
+    path('register.html', views.register, name='register'),
+    path('tables.html', views.tables, name='tables'),
+    path('utilities-animation.html', views.utilities_animation, name='utilities_animation'),
+    path('utilities-border.html', views.utilities_border, name='utilities_border'),
+    path('utilities-color.html', views.utilities_color, name='utilities_color'),
+    path('utilities-other.html', views.utilities_other, name='utilities_other'),
+
+    path('blank', views.blank, name='blank'),
+    path('buttons', views.buttons, name='buttons'),
+    path('cards', views.cards, name='cards'),
+    path('charts', views.charts, name='charts'),
+    path('forgot-password', views.forgot_password, name='forgot_password'),
+    path('login', views.login, name='login'),
+    path('register', views.register, name='register'),
+    path('tables', views.tables, name='tables'),
+    path('utilities-animation', views.utilities_animation, name='utilities_animation'),
+    path('utilities-border', views.utilities_border, name='utilities_border'),
+    path('utilities-color', views.utilities_color, name='utilities_color'),
+    path('utilities-other', views.utilities_other, name='utilities_other'),
+
     path('', views.index, name='home'),
+    path('index', views.index, name='home'),
+    path('index.html', views.index, name='home'),
     path('generate', views.generate, name='generate'),
     path('delete', views.delete, name='delete'),
 ]
--- fizuapp/views.py
+++ fizuapp/views.py
@@ -13,9 +13,71 @@ def generate_bg():
     time.sleep(2) # Simulate expensive operation.
     Secret.objects.create()
 
+
 def index(request):
     context = {'secrets': Secret.objects.all()}
     return render(request, 'fizuapp/index.html', context)
+
+
+def blank(request):
+    context = {'secrets': Secret.objects.all()}
+    return render(request, 'fizuapp/blank.html', context)
+
+
+def buttons(request):
+    context = {'secrets': Secret.objects.all()}
+    return render(request, 'fizuapp/buttons.html', context)
+
+
+def cards(request):
+    context = {'secrets': Secret.objects.all()}
+    return render(request, 'fizuapp/cards.html', context)
+
+
+def charts(request):
+    context = {'secrets': Secret.objects.all()}
+    return render(request, 'fizuapp/charts.html', context)
+
+
+def forgot_password(request):
+    context = {'secrets': Secret.objects.all()}
+    return render(request, 'fizuapp/forgot_password.html', context)
+
+
+def login(request):
+    context = {'secrets': Secret.objects.all()}
+    return render(request, 'fizuapp/login.html', context)
+
+
+def register(request):
+    context = {'secrets': Secret.objects.all()}
+    return render(request, 'fizuapp/register.html', context)
+
+
+def tables(request):
+    context = {'secrets': Secret.objects.all()}
+    return render(request, 'fizuapp/tables.html', context)
+
+
+def utilities_animation(request):
+    context = {'secrets': Secret.objects.all()}
+    return render(request, 'fizuapp/utilities_animation.html', context)
+
+
+def utilities_border(request):
+    context = {'secrets': Secret.objects.all()}
+    return render(request, 'fizuapp/utilities_border.html', context)
+
+
+def utilities_color(request):
+    context = {'secrets': Secret.objects.all()}
+    return render(request, 'fizuapp/utilities_color.html', context)
+
+
+def utilities_other(request):
+    context = {'secrets': Secret.objects.all()}
+    return render(request, 'fizuapp/utilities_other.html', context)
+
 
 def generate(request):
     if request.GET.get('bg'):
@@ -25,6 +87,7 @@ def generate(request):
         Secret.objects.create()
         messages.success(request, 'Generated new key.')
     return redirect('home')
+
 
 def delete(request):
     Secret.objects.all().delete()
